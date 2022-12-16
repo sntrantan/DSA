65993378e59e7e3e2d1a1ed8b0b74c329f84a1b8
Tan Tran <sn.trantan.work@gmail.com>
2022-12-16 12:29:40 -0600

    Comments for QuickSort

--- src/main/java/com/fundamentals/dsa/algorithms/QuickSort.java
+++ src/main/java/com/fundamentals/dsa/algorithms/QuickSort.java
@@ -3,6 +3,22 @@ package com.fundamentals.dsa.algorithms;
 import static com.fundamentals.dsa.algorithms.ArrayHelper.*;
 import static com.fundamentals.dsa.algorithms.ArrayHelper.printArray;
 
+/*
+     https://www.programiz.com/dsa/quick-sort
+     https://algs4.cs.princeton.edu/cheatsheet/
+     https://www.youtube.com/watch?v=COk73cpQbFQ
+     Quick Sort - recursive divide & conquer algorithm where we
+        1. divide the original array into multiple subarrays
+            a. while dividing, elements less than the pivot should be on the left & elements greater than the pivots should be on the right
+        2. recursively call, excluding the pivot index from future sorts
+     Best - O(n*log(n)) -- occurs when the pivot index is always the middle element / near the middle element
+     Worst - O(n^2) -- occurs when the pivot index is always the smallest or largest element
+     Average - O(n*log(n))
+     Space - O(n)
+     Stable - No -- due to the nature of swapping & placing pivot
+     In Place - Yes
+ */
+
 public class QuickSort {
 
     public static void quickSort(int[] nums, int low, int high){
